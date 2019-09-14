ef32657030ad2fc57b3c44b9e7e240abbcfca475
Mikael Remes <pojankolli@gmail.com>
2019-09-14 18:25:22 +0300

    Added playercollision with walls

--- GameDevGame/Assets/Scripts/PlayerControl.cs
+++ GameDevGame/Assets/Scripts/PlayerControl.cs
@@ -4,30 +4,31 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    void Update()
+    void FixedUpdate()
     {
         //player
         GameObject player = GameObject.Find("Player");
+        float speed = 0.2f;
 
         //player presses left
         if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            player.transform.position += Vector3.left*0.5f;
+            player.transform.position += Vector3.left* speed;
         }
         //player presses up
         if (Input.GetKey("w") || Input.GetKey("up"))
         {
-            player.transform.position += Vector3.forward * 0.5f;
+            player.transform.position += Vector3.forward * speed;
         }
         //player presses right
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            player.transform.position += Vector3.right * 0.5f;
+            player.transform.position += Vector3.right * speed;
         }
         //player presses down
         if (Input.GetKey("s") || Input.GetKey("down"))
         {
-            player.transform.position += Vector3.back * 0.5f;
+            player.transform.position += Vector3.back * speed;
         }
     }
 }
