2c824f743556538c050b27231cbb5778df1828b1
Mikael Remes <pojankolli@gmail.com>
2019-09-19 17:10:21 +0300

    Changed game completely to planet TD

---
+++ GameDevGame/Assets/ButtonColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonColor : MonoBehaviour
+{
+    private bool pressedState = false;
+
+    void Start()
+    {
+        GetComponent<Image>().color = Color.red;
+    }
+    public void ChangeColor()
+    {
+        if (pressedState)
+        {
+            GetComponent<Image>().color = Color.red;
+            pressedState = false;
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.green;
+            pressedState = true;
+        }
+    }
+}
---
+++ GameDevGame/Assets/Scripts/CameraViewScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewScript : MonoBehaviour
+{
+    public Vector3 planetViewPosition = new Vector3(0,5,-52);
+    public Quaternion planetViewRotation =  Quaternion.Euler(5, 0, 0);
+    public Vector3 orbitViewPosition = new Vector3(0,170,-170);
+    public Quaternion orbitViewRotation = Quaternion.Euler(45,0,0);
+    public int cameraState = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.SetPositionAndRotation(planetViewPosition, planetViewRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            transform.SetPositionAndRotation(planetViewPosition, planetViewRotation);
+            cameraState = 0;
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            transform.SetPositionAndRotation(orbitViewPosition, orbitViewRotation);
+            cameraState = 1;
+        }
+    }
+}
