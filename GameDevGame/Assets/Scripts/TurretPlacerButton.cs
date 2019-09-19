ce98981e36b618c7205b71e4b535d9f4a36a63d8
Mikael Remes <pojankolli@gmail.com>
2019-09-19 19:55:32 +0300

    Tried so hard

--- GameDevGame/Assets/Scripts/TurretPlacerButton.cs
+++ GameDevGame/Assets/Scripts/TurretPlacerButton.cs
@@ -5,36 +5,62 @@ using UnityEngine.UI;
 
 public class TurretPlacerButton : MonoBehaviour
 {
-    private bool pressed;
+    private bool placingTurret=false;
+    
+    public GameObject planet;
+    public GameObject basicTurret;
+    public Camera cameraView;
 
     void Awake()
     {
         GetComponent<Image>().color = Color.red;
-        pressed = TurretPlacerV2.placingTurret;
-        if (!pressed) GetComponent<Image>().color = Color.red;
+        if (!placingTurret) GetComponent<Image>().color = Color.red;
         else GetComponent<Image>().color = Color.green;
     }
 
     public void PressedButton()
     {
-        if (pressed)
+        if (placingTurret)
         {
             GetComponent<Image>().color = Color.red;
-            TurretPlacerV2.placingTurret = false;
-            pressed = false;
         }
         else
         {
             GetComponent<Image>().color = Color.green;
-            TurretPlacerV2.placingTurret = true;
-            pressed = true;
         }
+        placingTurret = !placingTurret;
     }
 
     public void DisableTurretPlacement()
     {
         GetComponent<Image>().color = Color.red;
-        TurretPlacerV2.placingTurret = false;
-        pressed = false;
+        placingTurret = false;
+    }
+
+    void Update()
+    {
+        if (placingTurret)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = cameraView.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    GameObject objectHit = hit.collider.gameObject;
+                    if (objectHit.Equals(planet))
+                    {
+                        placeTurret(hit.transform);
+                    }
+                }
+            }
+        }
+    }
+
+    private void placeTurret(Transform spot)
+    {
+        GameObject currentTurret = Instantiate(basicTurret);
+        currentTurret.transform.position = spot.position;
+        currentTurret.transform.rotation = spot.rotation;
     }
 }
