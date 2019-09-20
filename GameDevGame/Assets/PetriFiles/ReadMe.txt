c15d33a69d05e26bb037638c4212a4dd594656e9
petrinie <55512651+petrinie@users.noreply.github.com>
2019-09-21 00:08:45 +0300

    Some improvements made 20.9. (didn´t change the original files)
    Some improvements made 20.9. (See PETRIFILES-FOLDER for ReadMe.txt, files and scene). Didn´t change the original files.
    
    - Rotation issue: if you first rotate eg. 180 degrees, and then start rotate, it is messed up. I fixed it by using a temporary handle for rotation.
    
    - Changed rotation so that player is rotating the camera around the planet, not just the planet with static camera. That way the whole universe (background) rotates also.
    
    - Added simple space skybox (to see the rotation of background)
    
    - Added simple texture for planet (to see the rotation)
    
    - Added simple turret (just to see the rotation of the gameobject)

---
+++ GameDevGame/Assets/PetriFiles/ReadMe.txt
@@ -0,0 +1,11 @@
+Some improvements made 20.9. (See PETRIFILES-FOLDER for files and scene):
+
+- Rotation issue: if you first rotate eg. 180 degrees, and then start rotate, it is messed up. I fixed it by using a temporary handle for rotation.
+
+- Changed rotation so that player is rotating the camera around the planet, not just the planet with static camera. That way the whole universe (background) rotates also.
+
+- Added simple space skybox (to see the rotation of background)
+
+- Added simple texture for planet (to see the rotation)
+
+- Added simple turret (just to see the rotation of the gameobject)
---
+++ GameDevGame/Assets/PetriFiles/RotatePlanetButton2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotatePlanetButton2 : MonoBehaviour
+{
+    private bool pressed;
+
+    void Awake()
+    {
+        GetComponent<Image>().color = Color.red;
+        pressed = RotatePlanet2.rotateEnabled2;
+        if (!pressed) GetComponent<Image>().color = Color.red;
+        else GetComponent<Image>().color = Color.green;
+    }
+
+    public void PressedButton()
+    {
+        if (pressed)
+        {
+            GetComponent<Image>().color = Color.red;
+            RotatePlanet2.rotateEnabled2 = false;
+            pressed = false;
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.green;
+            RotatePlanet2.rotateEnabled2 = true;
+            pressed = true;
+        }
+    }
+
+    public void DisablePlanetRotation()
+    {
+        GetComponent<Image>().color = Color.red;
+        RotatePlanet2.rotateEnabled2 = false;
+        pressed = false;
+    }
+}
