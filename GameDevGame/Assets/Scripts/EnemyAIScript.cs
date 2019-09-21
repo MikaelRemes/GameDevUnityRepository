a1b09043038fd5ce0bf40c506dd3ba8bc817b1d8
Mikael Remes <pojankolli@gmail.com>
2019-09-21 15:28:18 +0300

    Added enemy with "AI"

--- GameDevGame/Assets/Scripts/EnemyAIScript.cs
+++ GameDevGame/Assets/Scripts/EnemyAIScript.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class EnemyAIScript : MonoBehaviour
 {
     public GameObject targetPlanet;
-    public float speed;
-    
-    void Update()
+    public float speed=0.05f;
+
+    void Start()
     {
-        //TODO: move towards target at speed * Time.deltatime
+        //move enemy towards player at speed x
+        //if speed can be slowed or planet can move, move this method to update
+        gameObject.GetComponent<Rigidbody>().velocity =
+            (targetPlanet.transform.position - gameObject.transform.position) * speed;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals(targetPlanet))
+        {
+            //TODO: deal damage to player
+            Destroy(gameObject);
+        }
     }
 }
