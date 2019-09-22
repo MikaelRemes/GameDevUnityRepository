7a6d16274c0eda71df31243a97f029afa9f50d02
Mikael Remes <pojankolli@gmail.com>
2019-09-22 12:28:03 +0300

    Turret now points at enemies

--- GameDevGame/Assets/Scripts/CameraRotator.cs
+++ GameDevGame/Assets/Scripts/CameraRotator.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class CameraRotator : MonoBehaviour
 {
     public float rotateSpeed = 1f;
+    public float zoomSpeed = 1f;
     public Camera playerCamera;
-    public Vector3 planetViewPosition = new Vector3(0, 0, -70);
-    public Vector3 orbitViewPosition = new Vector3(0, 0, -260);
-
-    private int cameraState = 0;
 
     // Update is called once per frame
     void Update()
@@ -34,23 +31,17 @@ public class CameraRotator : MonoBehaviour
         }
         if (Input.GetKey("q"))
         {
-            //currently changing view resets position
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            playerCamera.transform.position = planetViewPosition;
-            playerCamera.transform.rotation = Quaternion.Euler(0, 0, 0);
-            cameraState = 0;
+            //currently changing zoom
+            Vector3 direction = transform.position - playerCamera.transform.position;
+            direction.Normalize();
+            playerCamera.transform.position -= direction;
         }
         if (Input.GetKey("e"))
         {
-            //currently changing view resets positions
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            playerCamera.transform.position = orbitViewPosition;
-            playerCamera.transform.rotation = Quaternion.Euler(0, 0, 0);
-            cameraState = 1;
-        }
-        if (Input.GetKeyDown("r"))
-        {
-            Debug.Log("" + (transform.localEulerAngles.y <= 90));
+            //currently changing zoom
+            Vector3 direction = transform.position - playerCamera.transform.position;
+            direction.Normalize();
+            playerCamera.transform.position += direction;
         }
     }
 }
---
+++ GameDevGame/Assets/Scripts/TurretAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAI : MonoBehaviour
+{
+    public float range=50f;
+    public string enemyTag = "Enemy";
+    public Transform rotateAxis;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f,0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            Vector3 directionOfEnemy = target.position - transform.position;
+            Quaternion lookDirection = Quaternion.LookRotation(directionOfEnemy);
+            rotateAxis.rotation = lookDirection;
+        }
+    }
+}
